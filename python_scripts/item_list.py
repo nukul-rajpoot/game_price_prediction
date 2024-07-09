2feed41e075f56dd58e9ecb63beb0be0cddadf7f
sohamjghosh <sohamjghosh@gmail.com>
2024-07-09 22:52:21 +0100

    added item list

---
+++ python_scripts/item_list.py
@@ -0,0 +1,29 @@
+import re
+import requests
+import pandas as pd
+import os
+
+def get_item_list():
+    url = "https://api.steamapis.com/market/items/730?api_key=uJHHr--KdQ-KdymIei9IdHJdMBQ"
+   
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+        json_data = response.json()
+        return json_data
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+
+json_data = get_item_list()
+if json_data:
+
+    get_item_list_df = pd.DataFrame(json_data)
+    print(get_item_list_df.head)
+    get_item_list_df.to_csv('B:\PRICE PREDICTOR\game_price_prediction\data/CSGO_Item_List.csv', index=True)
+  
+else:
+    print("Failed to retrieve data")
+
