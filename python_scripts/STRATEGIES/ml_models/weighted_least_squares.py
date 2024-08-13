ef7f369669cb4769bb2acc023e176b555e74b3ad
sohamjghosh <sohamjghosh@gmail.com>
2024-08-13 17:03:14 +0100

    working lstm model with incorrect metrics + fixed import statements in wls

--- python_scripts/STRATEGIES/ml_models/weighted_least_squares.py
+++ python_scripts/STRATEGIES/ml_models/weighted_least_squares.py
@@ -1,30 +1,44 @@
-# from sklearn.linear_model import LinearRegression
-# from sklearn.model_selection import train_test_split
-# import statsmodels.api as sm
-# import plotly.graph_objects as go
-# import pandas as pd
-# from ipywidgets import interact, DatePicker
-# import numpy as np
-# from make_graphs import plot_weighted_least_squares
 
-# from api_calls import fetch_item_from_api, fetch_item_to_df, fetch_daily_cookie, fetch_items
-
+import statsmodels.api as sm
+import pandas as pd
+from ipywidgets import interact, DatePicker
+import numpy as np
 # #api call and fetching dataframe
+import sys
+import os
+current_dir = os.path.abspath(os.curdir)
+sys.path.insert(0, current_dir)
+from python_scripts.api_calls import fetch_item_to_df, fetch_items, get_cookie_from_blob
+from python_scripts.VISUALISATION.make_graphs import plot_weighted_least_squares
 
-
-dailyCookie = fetch_daily_cookie()
+dailyCookie = get_cookie_from_blob()
 items = fetch_items()
+
 current_item = fetch_item_to_df(items[4], dailyCookie)
+#print(items[4])
+
+#print(current_item.tail())
+#print(non_aggregated_item.tail())
+
+df = current_item
+
+os. getcwd()
+
+# #dailyCookie = fetch_daily_cookie()
+# #items = fetch_items()
+# #current_item = fetch_item_to_df(items[4], dailyCookie)
 
 
-df = current_item.copy()
+# #df = current_item.copy()
 
 # Weights = determine the influence of data points in WLS; using weights of 1,1 yields OLS results (OLS has uniform weightings).
 
 df['date_ordinal'] = df.index.map(pd.Timestamp.toordinal)
+
 X = df['date_ordinal'].values.reshape(-1, 1)
 y = df['price_usd'].values
-
+print((y))
+print((X))
 weights = np.linspace(1, 10, len(X))
 
 model_wls = sm.WLS(y, sm.add_constant(X), weights=weights).fit()
@@ -41,7 +55,7 @@ start_date_picker = DatePicker(description='Start Date', value=df.index[0], disa
 end_date_picker = DatePicker(description='End Date', value=df.index[-1], disabled=False)
 
 
-# interact(lambda start_date, end_date:
-#             plot_weighted_least_squares(df, start_date, end_date, y_pred, split_point, y_test),
-#             start_date=start_date_picker, end_date=end_date_picker)
+interact(lambda start_date, end_date:
+            plot_weighted_least_squares(df, start_date, end_date, y_pred, split_point, y_test),
+            start_date=start_date_picker, end_date=end_date_picker)
 
