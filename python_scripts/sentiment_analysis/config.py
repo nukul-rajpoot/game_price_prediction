0679949c52188cc3bc8b5c90c7f01cdd3ca4f19a
DabMaster22 <Dabdab22@gmail.com>
2024-10-05 23:59:28 +0100

    All necessary scripts now iterate over the ITEMS list of mispelled items

--- python_scripts/sentiment_analysis/config.py
+++ python_scripts/sentiment_analysis/config.py
@@ -6,18 +6,26 @@ NOTE: sets word to analyse universally through the folowing:
 > mention_data_combiner
 > vader_polarity
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""" 
-# input the skibbity item's name here :P 
-ITEM = "redline"
+# input the skibbity item's name here :P  NOTE: ITEMS = [] for mispelled items
+ITEM = "Glove Case"
+ITEMS = [
+    "Glove Case", "Golve Cace", "Golve Cse", "Golve Casse", "Golve Kase", "Golve Caes", "Golve Cas", "Golve Cease", "Golve Casa", "Golve Case", "Golve Casw",
+    "Glov Cace", "Glov Cse", "Glov Casse", "Glov Kase", "Glov Caes", "Glov Cas", "Glov Cease", "Glov Casa", "Glov Case", "Glov Casw",
+    "Glowe Cace", "Glowe Cse", "Glowe Casse", "Glowe Kase", "Glowe Caes", "Glowe Cas", "Glowe Cease", "Glowe Casa", "Glowe Case", "Glowe Casw", "Glove box"
+    ]
 
-# For filter_file               |    NOTE: (input: compressed -> output: filtered_data)
+# For filter_file               |    NOTE: (input: compressed -> output: filtered_data (MS) )
 INPUT_COMPRESSED = './data/Reddit_data/compressed_data'
 FILTERED_DATA_DIRECTORY = f'./data/Reddit_data/filtered_data/{ITEM}_from_compressed_data'
 
-# for mention_counter           |   NOTE:  (input: compressed -> output: mention_data)
+# for mention_counter           |   NOTE:  (input: filtered_data (MS) -> output: mention_data (MS))
 MENTION_DATA_DIRECTORY = f'./data/Reddit_data/mention_data/{ITEM}_from_compressed_data'
 
-# mention_data_combiner         |   NOTE:  (input: mention_data -> output: mention_ALL)  - directories N/A for this.
+# mention_data_combiner         |   NOTE:  (input: mention_data (MS) -> output: mention_ALL)  - directories N/A for this.
 
 # For vader_polarity            |    NOTE: (input: filtered -> output: polarity_data)
 POLARITY_DATA_DIRECTORY = f'./data/Reddit_data/polarity_data/{ITEM}'
 OUTPUT_POLARITY_FORMAT = "csv"
+
+# note: if it handles mispelled ITEMS=[], then denoted as MS
+# print(ITEMS)
