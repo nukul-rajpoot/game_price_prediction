78c28cd649eccc3efe8bb7d9fc267b511b6c2850
nukul-rajpoot <61462716+nukul-rajpoot@users.noreply.github.com>
2024-10-19 01:34:14 +0100

    Refactored code to define paths and names in one place.

--- python_scripts/sentiment_analysis/config.py
+++ python_scripts/sentiment_analysis/config.py
@@ -6,8 +6,24 @@ NOTE: sets word to analyse universally through the folowing:
 > mention_data_combiner
 > vader_polarity
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""" 
+
+import os,sys
+cwd = os.getcwd()
+print(cwd)
+
+# # for windowpros
+#game_price_prediction_path = os.path.abspath(os.path.join(cwd, '..', '..'))
+
+# # for ma:c
+GAME_PRICE_PREDICTION_PATH = os.path.abspath(os.path.join(cwd))
+
+sys.path.insert(0, os.path.abspath(GAME_PRICE_PREDICTION_PATH))
+
+from python_scripts.utilities.api_calls import sanitize_filename
+
 # input the skibbity item's name here :P  NOTE: ITEMS = [] for mispelled items
 ITEM = "Glove Case"
+ITEM_SANITIZED = sanitize_filename(ITEM)
 ITEMS = [
     "Glove Case", "Golve Cace", "Golve Cse", "Golve Casse", "Golve Kase", "Golve Caes", "Golve Cas", "Golve Cease", "Golve Casa", "Golve Case", "Golve Casw",
     "Glov Cace", "Glov Cse", "Glov Casse", "Glov Kase", "Glov Caes", "Glov Cas", "Glov Cease", "Glov Casa", "Glov Case", "Glov Casw",
@@ -15,16 +31,19 @@ ITEMS = [
     ]
 
 # For filter_file               |    NOTE: (input: compressed -> output: filtered_data (MS) )
-INPUT_COMPRESSED = './data/Reddit_data/compressed_data'
-FILTERED_DATA_DIRECTORY = f'./data/Reddit_data/filtered_data/{ITEM}_from_compressed_data'
+INPUT_COMPRESSED = os.path.join(GAME_PRICE_PREDICTION_PATH, 'data', 'Reddit_data', 'compressed_data')
+FILTERED_DATA_DIRECTORY = os.path.join(GAME_PRICE_PREDICTION_PATH, 'data', 'Reddit_data', 'filtered_data', f'{ITEM_SANITIZED}_filtered')
 
 # for mention_counter           |   NOTE:  (input: filtered_data (MS) -> output: mention_data (MS))
-MENTION_DATA_DIRECTORY = f'./data/Reddit_data/mention_data/{ITEM}_from_compressed_data'
+MENTION_DATA_DIRECTORY = os.path.join(GAME_PRICE_PREDICTION_PATH, 'data', 'Reddit_data', 'mention_data', f'{ITEM_SANITIZED}_mentions')
 
 # mention_data_combiner         |   NOTE:  (input: mention_data (MS) -> output: mention_ALL)  - directories N/A for this.
+ALL_MENTIONS_FILENAME = f'{ITEM_SANITIZED}_all_mentions.csv'
+ALL_MENTIONS_DATA = os.path.join(GAME_PRICE_PREDICTION_PATH, 'data', 'Reddit_data', 'mention_all', ALL_MENTIONS_FILENAME)
 
 # For vader_polarity            |    NOTE: (input: filtered -> output: polarity_data)
-POLARITY_DATA_DIRECTORY = f'./data/Reddit_data/polarity_data/{ITEM}'
+POLARITY_FOLDER_NAME = f'{ITEM_SANITIZED}_polarity'
+POLARITY_DATA_DIRECTORY = os.path.join(GAME_PRICE_PREDICTION_PATH, 'data', 'Reddit_data', 'polarity_data', POLARITY_FOLDER_NAME)
 OUTPUT_POLARITY_FORMAT = "csv"
 
 # note: if it handles mispelled ITEMS=[], then denoted as MS
--- python_scripts/sentiment_analysis/mention_data_combiner.py
+++ python_scripts/sentiment_analysis/mention_data_combiner.py
@@ -1,17 +1,18 @@
 import os
 import pandas as pd
-from config import ITEM, MENTION_DATA_DIRECTORY
+from config import ITEM, MENTION_DATA_DIRECTORY, ALL_MENTIONS_DATA
 
 # Directory containing the mention data CSV files
-mention_data_directory = MENTION_DATA_DIRECTORY
+input_file = MENTION_DATA_DIRECTORY
+output_file = ALL_MENTIONS_DATA
 
 # List to store individual dataframes
 dataframes = []
 
 # Iterate over each file in the mention_data directory
-for file_name in os.listdir(mention_data_directory):
+for file_name in os.listdir(input_file):
     if file_name.endswith('.csv'):
-        file_path = os.path.join(mention_data_directory, file_name)
+        file_path = os.path.join(input_file, file_name)
         df = pd.read_csv(file_path)
         dataframes.append(df)
 
@@ -28,6 +29,5 @@ summed_df = combined_df.groupby('date', as_index=False)['num_mentions'].sum()
 summed_df = summed_df.sort_values(by='date')
 
 # Skibbity sigma rizz
-output_file = f'./data/Reddit_data/mention_all/{ITEM}_from_all.csv'
 summed_df.to_csv(output_file, index=False)
 print(f"ALL available mention_data FOR {ITEM} has been combined into {output_file}")
