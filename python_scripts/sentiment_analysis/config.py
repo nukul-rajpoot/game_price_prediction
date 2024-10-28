921f85df171156429a4b1a7ac3bcc39f71d17c2f
Nukul Rajpoot <nukulrajpoot@gmail.com>
2024-10-28 13:05:26 +0000

    updated scripts to run on all compressed data

--- python_scripts/sentiment_analysis/config.py
+++ python_scripts/sentiment_analysis/config.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath(GAME_PRICE_PREDICTION_PATH))
 from python_scripts.utilities.api_calls import sanitize_filename
 
 # input the skibbity item's name here :P  NOTE: ITEMS = [] for mispelled items
-#ITEM = "ALL DATA"
-ITEM = "M4A1-S | Golden Coil (Factory New)"
+ITEM = "ALL DATA"
+#ITEM = "M4A1-S | Golden Coil (Factory New)"
 ITEM_SANITIZED = sanitize_filename(ITEM)
 ITEMS = [
     "M4A1-S | Golden Coil", "Golden Coil", "GoldenCoil", "M4A1-S", "M4A1S", 
